f62deb5f6383f2f174a2796d5964475befb11a45
RISC-1145 <wtpp647@gmail.com>
2026-08-11 00:31:41 +0800

    Initial update

---
+++ redc_main.c
@@ -0,0 +1,81 @@
+﻿#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+#include "redc_math.h"
+
+#define WINDOW_WIDTH 640
+#define WINDOW_HEIGHT 480
+
+static SDL_Renderer *renderer = nullptr;
+
+
+static void draw_pixel(struct vec2f pos, struct vec3i color) {
+    SDL_SetRenderDrawColor(renderer, color.x, color.y, color.z, 255);
+    SDL_RenderPoint(renderer, pos.x, pos.y);
+}
+
+static void draw_line_dda(struct vec2f start, struct vec2f end, struct vec3i color) {
+    const struct vec2f delta = sub_vec2f(end, start);
+    const int steps = max((int) fabsf(delta.x), (int) fabsf(delta.y));
+    const struct vec2f increment = div_vec2f_scaler(delta, (float) steps);
+
+    struct vec2f temp_pos = start;
+    for (int i = 0; i <= steps; i++) {
+        draw_pixel(temp_pos, color);
+        temp_pos = add_vec2f(temp_pos, increment);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_Log("SDL_Init Error: %s", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window *window = nullptr;
+    if (!SDL_CreateWindowAndRenderer("RedC Renderer", WINDOW_WIDTH, WINDOW_HEIGHT, 0, &window, &renderer)) {
+        SDL_Log("Window/Renderer Creation Error: %s", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    bool running = true;
+    SDL_Event event;
+
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) {
+                running = false;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        for (int i = 0; i < 128; i++) {
+            struct vec2f start = {
+                .x = gen_random_float_range(0.0f, (float) WINDOW_WIDTH),
+                .y = gen_random_float_range(0.0f, (float) WINDOW_HEIGHT)
+            };
+            struct vec2f end = {
+                .x = gen_random_float_range(0.0f, (float) WINDOW_WIDTH),
+                .y = gen_random_float_range(0.0f, (float) WINDOW_HEIGHT)
+            };
+            struct vec3i color = {
+                .x = gen_random_int_range(0, 256),
+                .y = gen_random_int_range(0, 256),
+                .z = gen_random_int_range(0, 256)
+            };
+
+            draw_line_dda(start, end, color);
+        }
+
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
---
+++ redc_math.h
@@ -0,0 +1,150 @@
+﻿#pragma once
+
+#include <math.h>
+#include <stdio.h>
+#include <windows.h>
+#include <ntsecapi.h>
+
+struct vec2f {
+    float x, y;
+};
+
+struct vec3f {
+    float x, y, z;
+};
+
+struct vec4f {
+    float x, y, z, w;
+};
+
+struct vec2i {
+    int x, y;
+};
+
+struct vec3i {
+    int x, y, z;
+};
+
+struct vec4i {
+    int x, y, z, w;
+};
+
+/*
+ *  ---------------- Vec2 Calculate ----------------
+ */
+
+inline static struct vec2f add_vec2f(struct vec2f v1, struct vec2f v2) {
+    struct vec2f result = {
+        .x = v1.x + v2.x,
+        .y = v1.y + v2.y
+    };
+    return result;
+}
+
+inline static struct vec2f sub_vec2f(struct vec2f v1, struct vec2f v2) {
+    struct vec2f result = {
+        .x = v1.x - v2.x,
+        .y = v1.y - v2.y
+    };
+    return result;
+}
+
+inline static struct vec2f mul_vec2f(struct vec2f v1, struct vec2f v2) {
+    struct vec2f result = {
+        .x = v1.x * v2.x,
+        .y = v1.y * v2.y
+    };
+    return result;
+}
+
+inline static struct vec2f div_vec2f(struct vec2f v1, struct vec2f v2) {
+    struct vec2f result = {
+        .x = v1.x / v2.x,
+        .y = v1.y / v2.y
+    };
+    return result;
+}
+
+inline static struct vec2f add_vec2f_scaler(struct vec2f v1, float v2) {
+    struct vec2f result = {
+        .x = v1.x + v2,
+        .y = v1.y + v2
+    };
+    return result;
+}
+
+inline static struct vec2f sub_vec2f_scaler(struct vec2f v1, float v2) {
+    struct vec2f result = {
+        .x = v1.x - v2,
+        .y = v1.y - v2
+    };
+    return result;
+}
+
+inline static struct vec2f mul_vec2f_scaler(struct vec2f v1, float v2) {
+    struct vec2f result = {
+        .x = v1.x * v2,
+        .y = v1.y * v2
+    };
+    return result;
+}
+
+inline static struct vec2f div_vec2f_scaler(struct vec2f v1, float v2) {
+    struct vec2f result = {
+        .x = v1.x / v2,
+        .y = v1.y / v2
+    };
+    return result;
+}
+
+/*
+ *  ---------------- Random ----------------
+ */
+
+static inline float gen_random_float(void) {
+    unsigned int r = 0;
+    if (RtlGenRandom(&r, sizeof(r)) == FALSE) {
+        fprintf(stderr, "RtlGenRandom failed\n");
+        return -1.0f;
+    }
+    return (float) r / (float) UINT32_MAX;
+}
+
+static inline float gen_random_float_range(float min, float max) {
+    if (max <= min) {
+        fprintf(stderr, "gen_random_float_range: invalid range\n");
+        return min;
+    }
+    unsigned int r = 0;
+    if (RtlGenRandom(&r, sizeof(r)) == FALSE) {
+        fprintf(stderr, "RtlGenRandom failed\n");
+        return min;
+    }
+    float normalized = (float) r / (float) UINT32_MAX;
+    return min + normalized * (max - min);
+}
+
+static inline int gen_random_int(void) {
+    unsigned int r = 0;
+    if (RtlGenRandom(&r, sizeof(r)) == FALSE) {
+        fprintf(stderr, "RtlGenRandom failed\n");
+        return -1;
+    }
+
+    return (int) ((float) r / (float) UINT32_MAX * (float) INT_MAX);
+}
+
+static inline int gen_random_int_range(int min, int max) {
+    if (max <= min) {
+        fprintf(stderr, "gen_random_int_range: invalid range\n");
+        return min;
+    }
+    unsigned int r = 0;
+    if (RtlGenRandom(&r, sizeof(r)) == FALSE) {
+        fprintf(stderr, "RtlGenRandom failed\n");
+        return min;
+    }
+    float normalized = (float) r / (float) UINT32_MAX;
+    int range = max - min;
+    return min + (int) (normalized * range);
+}
